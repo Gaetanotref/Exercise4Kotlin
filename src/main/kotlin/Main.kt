e74eac412322d9f2f6b153aaec42cc70c4711695
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-20 16:49:32 +0100

    Update: methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
 fun main(args: Array<String>) {
     for (i in 1..100 step 2) {
         when (i) {
-            in 1..50 -> println("This is the first print and the number is ${i}!")
-            in 50..100 -> println("This is the second print and the number is ${i}!")
+            in 1..50 -> Above50(i)
+            in 50..100 -> Over50(i)
         }
     }
+}
+
+fun Above50(i: Int) {
+    println("This is the first print and the number is ${i}!")
+}
+
+fun Over50(i: Int) {
+    println("This is the second print and the number is ${i}!")
 }
